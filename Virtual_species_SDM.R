46f07b71ed75ff0fbbeb7061b613eeb7c509a5a4
Duccio Rocchini <ducciorocchini@gmail.com>
2022-11-21 06:49:53 +0100

    Update Virtual_species_SDM.R

--- Virtual_species_SDM.R
+++ Virtual_species_SDM.R
@@ -4,9 +4,6 @@
 # Original code by: Elisa Marchetto
 # Revised code by: Jakub Nowosad and Duccio Rocchini
 
-### R code for generating the virtual species and communities presented in:
-### Rocchini et al. (2022). A quixotic view of spatial bias in modelling the distribution of species and their diversity
-
 # packages installation ---------------------------------------------------
 list.of.packages <- c("raster", "tidyverse", "sp", "virtualspecies", "ggplot2",
                       "terra", "sf", "fuzzySim", "rasterVis", "viridis", "colorist",
